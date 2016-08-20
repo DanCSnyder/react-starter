980717bb3b378c6caba498b6c9e5195b7d06a62a
Tj Carskadon <tjcarskadon@gmail.com>
2016-08-20 10:50:32 -0700

    refactor to use query string on multiple loaders

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,12 @@ module.exports = {
   },
   module: {
     loaders: [
-      {loader: 'babel-loader', test: /\.js?$/, exclude: /node_modules/, query: { presets: ["es2015", "react"] }},
-      {test: /\.js?$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/},
+      {test: /\.js?$/, loaders: ['react-hot', 'babel-loader?'  + JSON.stringify({presets: ["es2015", "react"]})] , exclude: /node_modules/},
       {test: /\css?$/, loader: 'style!css'}
     ]
   },
   plugins: [
-    new webpack.NoErrorsPlugin()
+    // new webpack.NoErrorsPlugin()
   ]
 
 };
